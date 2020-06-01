0f2b2d7e1ec740887538445eb608c65c98376878
sft3hy <sft3hy@virginia.edu>
2020-06-01 15:48:02 -0400

    Resposive header and subheader

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="center">
+      <h1 class="full-text">Sam Townsend Journal</h1>
+      <h1 class="short-text">STJ</h1>
+      <h2 className="SubHeader">
+        Home  World  U.S.  Politics  Economy  Business  Tech  Markets  Opinion  Life and Arts  Real Estate  WSJ Magazine
+        </h2>
+    </div>
+
+  );
+}
+
+export default App;
